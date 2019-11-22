b1d0c411cd42aea3b7590ffb5428b903dceeb548
Jithin Sebastian <jithinsebastian@pop-os.localdomain>
2019-11-22 17:49:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MIUI Debloater
+
+Removes MIUI apps not needed by most.
+
+## Usage
+- Clone the repo
+- Run `sh script.sh`
+
+Note: Make sure you have platform tools setup. See:https://developer.android.com/studio/releases/platform-tools
---
+++ script.sh
@@ -0,0 +1,43 @@
+adb shell pm uninstall -k --user 0 com.android.browser #Mi Browser
+adb shell pm uninstall -k --user 0 com.android.calendar #Calendario
+adb shell pm uninstall -k --user 0 com.android.deskclock #Reloj
+adb shell pm uninstall -k --user 0 com.android.mms #Mensajes
+adb shell pm uninstall -k --user 0 com.facebook.katana #Facebook
+adb shell pm uninstall -k --user 0 com.facebook.appmanager #Facebook App Manager
+adb shell pm uninstall -k --user 0 com.facebook.services #Facebook Services
+adb shell pm uninstall -k --user 0 com.facebook.system #Facebook App Installer
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs #Google Drive
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon #Google Duo
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox #Google App
+adb shell pm uninstall -k --user 0 com.google.android.inputmethod.latin #Gboard
+adb shell pm uninstall -k --user 0 com.google.android.music #Google Music
+adb shell pm uninstall -k --user 0 com.google.android.videos #Play Movies
+adb shell pm uninstall -k --user 0 com.google.android.youtube #Youtube
+adb shell pm uninstall -k --user 0 com.mi.android.globalFileexplorer #Administrador de archivos
+adb shell pm uninstall -k --user 0 com.miui.analytics #Analytics
+adb shell pm uninstall -k --user 0 com.miui.bugreport #Mi Feedback
+adb shell pm uninstall -k --user 0 com.miui.calculator #Calculadora
+adb shell pm uninstall -k --user 0 com.miui.compass #Mi Compass
+adb shell pm uninstall -k --user 0 com.miui.msa.global #mas (main system advertising)
+adb shell pm uninstall -k --user 0 com.miui.notes #Mi Notes
+adb shell pm uninstall -k --user 0 com.miui.player #Mi Music
+adb shell pm uninstall -k --user 0 com.miui.screenrecorder #Mi Screen Recorder
+adb shell pm uninstall -k --user 0 com.miui.videoplayer #Mi Video
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop #Mi Drop
+adb shell pm uninstall -k --user 0 com.xiaomi.mipicks #Mi Apps
+adb shell pm uninstall -k --user 0 com.xiaomi.scanner #Mi Scanner
+adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery #Wallpaper Carousel
+adb shell pm uninstall -k --user 0 com.miui.weather2 #Mi Weather
+adb shell pm uninstall -k --user 0 com.miui.miservice #Mi Service
+adb shell pm uninstall -k --user 0 com.android.soundrecorder #Mi Sound Recorder
+adb shell pm uninstall -k --user 0 net.one97.paytm #Paytm
+adb shell pm uninstall -k --user 0 com.mipay.wallet.in #Mi Wallet
+adb shell pm uninstall -k --user 0 cn.wps.xiaomi.abroad.lite #Mi Doc
+adb shell pm uninstall -k --user 0 com.micredit.in #Mi Credit
+adb shell pm uninstall -k --user 0 com.gaana #Gaana
+adb shell pm uninstall -k --user 0 com.eterno #Dailyhunt
+adb shell pm uninstall -k --user 0 in.amazon.mShop.android.shopping #Amazon
+adb shell pm uninstall -k --user 0 com.miui.yellowpage #Mi Yellow Page
+adb shell pm uninstall -k --user 0 com.xiaomi.payment #Mi Payment
+adb shell pm uninstall -k --user 0 com.miui.hybrid #Quick Apps
+adb shell pm uninstall -k --user 0 com.mi.android.globalpersonalassistant #App vault
